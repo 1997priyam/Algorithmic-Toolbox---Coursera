913934457a770f0a5852a13119b3f6965612d65e
1997priyam <1997priyam@gmail.com>
2019-10-04 23:22:17 +0530

    Larges number / maximum salary- greedy algo

--- week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
@@ -7,8 +7,21 @@
 using std::vector;
 using std::string;
 
+int myCompare(string X, string Y) 
+{ 
+    // first append Y at the end of X 
+    string XY = X.append(Y); 
+  
+    // then append X at the end of Y 
+    string YX = Y.append(X); 
+  
+    // Now see which of the two formed numbers is greater 
+    return XY.compare(YX) > 0 ? 1: 0; 
+} 
+
 string largest_number(vector<string> a) {
   //write your code here
+  sort(a.begin(), a.end(), myCompare);
   std::stringstream ret;
   for (size_t i = 0; i < a.size(); i++) {
     ret << a[i];

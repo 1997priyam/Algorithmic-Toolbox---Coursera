a8623b2ff0f6482b85951417b5513548f63efc57
1997priyam <1997priyam@gmail.com>
2019-10-08 10:59:32 +0530

    majority element

--- week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -1,13 +1,26 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-
-using std::vector;
+#include <unordered_map>
+#include <bits/stdc++.h> 
+using namespace std;
 
 int get_majority_element(vector<int> &a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
   //write your code here
+  std::unordered_map<int, int> lookup;
+  for(int i=0; i<a.size(); i++){
+    if ( lookup.find(a[i]) == lookup.end() ) {
+      lookup[a[i]] = 1;
+    } else {
+      lookup[a[i]] += 1; 
+    }
+  }
+  int threshold = a.size()/2;
+  for(int i=0; i<a.size(); i++){
+    if(lookup[a[i]] > threshold) return 1;
+  }
   return -1;
 }
 

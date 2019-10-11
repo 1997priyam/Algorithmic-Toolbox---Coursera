3e0361999d60363cc8ee3fe05151dd22f8137b92
1997priyam <1997priyam@gmail.com>
2019-10-11 17:43:01 +0530

    Dp week 6

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-
-using std::vector;
+#include <algorithm>
+using namespace std;
 
 int optimal_weight(int W, const vector<int> &w) {
   //write your code here
@@ -14,6 +14,28 @@ int optimal_weight(int W, const vector<int> &w) {
   return current_weight;
 }
 
+int dp_optimal_weight(int W, const vector<int> &w) {
+  vector< vector<int> > v(w.size()+1, vector<int>(W+1));
+  for (int i = 0; i <= w.size(); i++) {
+  	v[i][0] = 0;
+  }
+  for (int i = 0; i <= W; i++) {
+    v[0][i] = 0;
+  }
+
+  for (int i = 1; i <= w.size(); i++) {
+  	for (int j = 1; j <= W; j++) {
+  	  if (j - w[i - 1] >= 0) {
+  	  	v[i][j] = max(v[i - 1][j], v[i - 1][j - w[i - 1]] + w[i - 1]);
+  	  } else {
+  	  	v[i][j] = v[i - 1][j];
+  	  }
+  	}
+  }
+
+  return v[w.size()][W];
+}
+
 int main() {
   int n, W;
   std::cin >> W >> n;
@@ -21,5 +43,5 @@ int main() {
   for (int i = 0; i < n; i++) {
     std::cin >> w[i];
   }
-  std::cout << optimal_weight(W, w) << '\n';
+  std::cout << dp_optimal_weight(W, w) << '\n';
 }
--- week6_dynamic_programming2/2_partitioning_souvenirs/partition3.cpp
+++ week6_dynamic_programming2/2_partitioning_souvenirs/partition3.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
 #include <vector>
-
-using std::vector;
+#include <bits/stdc++.h>
+using namespace std;
 
 int partition3(vector<int> &A) {
   //write your code here
   return 0;
 }
 
+bool canPartition(vector<int>& nums, int k) 
+{
+    int sum0=accumulate(nums.begin(),nums.end(),0);
+    if(sum0%k) return 0;
+    //discrete knapsack without repeti 
+    int n=nums.size();
+    vector<vector<int> > value(sum0/k+1,vector<int>(n+1));
+    for(int i=1;i<=n;i++)
+    {
+        int wi=nums[i-1];
+        //cout<<wi<<endl;
+        for(int w=1;w<=sum0/k;w++) //all smaller problems
+        {
+            value[w][i]=value[w][i-1];//if w>wi then it use previous value
+            if(w>=wi)
+            {
+                value[w][i]=max(value[w-wi][i-1]+wi,value[w][i-1]);                    
+            }
+        }
+     }
+    return value[sum0/k][n]==sum0/k; 
+	}
+
 int main() {
-  int n;
+  int n, k=3;
   std::cin >> n;
   vector<int> A(n);
   for (size_t i = 0; i < A.size(); ++i) {
     std::cin >> A[i];
   }
-  std::cout << partition3(A) << '\n';
+  std::cout << canPartition(A, k) << '\n';
 }

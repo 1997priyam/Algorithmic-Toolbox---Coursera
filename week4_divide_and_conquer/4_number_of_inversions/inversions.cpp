d8ada1286dae03599cd14afc3336ae14137d2f81
1997priyam <1997priyam@gmail.com>
2019-10-11 10:21:02 +0530

    Dynamic programming more

--- week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
+++ week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
@@ -3,13 +3,45 @@
 
 using std::vector;
 
-long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
+long long merge(vector<int> &a, vector<int> &b, int left, int ave, int right) {
+    int i = left, j = ave, k = left;
+    long long inv_count = 0;
+    while (i <= ave - 1 && j <= right) {
+        if (a[i] <= a[j]) {
+            b[k] = a[i];
+            i++;
+        } else {
+            b[k] = a[j];
+            inv_count += ave - i;
+            j++;
+        }
+        k++;
+    }
+    while (i <= ave - 1) {
+        b[k] = a[i];
+        i++;
+        k++;
+    }
+    while (j <= right) {
+        b[k] = a[j];
+        j++;
+        k++;
+    }
+    for (i = left; i <= right; i++) {
+        a[i] = b[i];
+    }
+    return inv_count;
+}
+
+long long get_number_of_inversions(vector<int> &a, vector<int> &b, int left, int right) {
   long long number_of_inversions = 0;
-  if (right <= left + 1) return number_of_inversions;
-  size_t ave = left + (right - left) / 2;
-  number_of_inversions += get_number_of_inversions(a, b, left, ave);
-  number_of_inversions += get_number_of_inversions(a, b, ave, right);
-  //write your code here
+  int ave = 0;
+  if(right > left){
+    ave = left + (right - left) / 2;
+    number_of_inversions += get_number_of_inversions(a, b, left, ave);
+    number_of_inversions += get_number_of_inversions(a, b, ave+1, right);
+    number_of_inversions += merge(a, b, left, ave+1, right);
+  }
   return number_of_inversions;
 }
 
@@ -17,7 +49,7 @@ int main() {
   int n;
   std::cin >> n;
   vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
+  for (int i = 0; i < a.size(); i++) {
     std::cin >> a[i];
   }
   vector<int> b(a.size());

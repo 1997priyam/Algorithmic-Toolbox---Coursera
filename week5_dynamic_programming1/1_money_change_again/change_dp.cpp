373d2ca4b3ec96f0de011dd03d6f29e04d5ead4a
1997priyam <1997priyam@gmail.com>
2019-10-11 10:17:33 +0530

    Dynamic programming

--- week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <vector>
+#include<bits/stdc++.h> 
+using namespace std;
 
 int get_change(int m) {
   //write your code here
-  return m / 4;
+  vector<int> M(m+1);
+  M[0] = 0;
+  for(int i=1; i<=m; i++){
+    int minimum = INT_MAX;
+    if(i>=1) minimum = min(minimum, 1+M[i-1]);
+    if(i>=3) minimum = min(minimum, 1+M[i-3]);
+    if(i>=4) minimum = min(minimum, 1+M[i-4]);
+    M[i] = minimum;
+  }
+  return M[m];;
 }
 
 int main() {
